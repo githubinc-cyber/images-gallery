ae2f4ebd7f63eab4763131b5e1240b54d9080e8f
githubinc-cyber <bilgekagan902@gmail.com>
2021-09-22 21:39:04 +0300

    test

---
+++ fronted/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <h1>Hello React</h1>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
